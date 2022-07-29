edb8d1c80c59fc8e53b06f057c101fe352639ac9
Gabriel de Azevedo <gmarquesazti@gmail.com>
2022-07-29 14:20:17 -0300

    fix: formatting

--- controllers/users.controllers/getOneUser.js
+++ controllers/users.controllers/getOneUser.js
@@ -3,6 +3,7 @@ import functions from '../../database/functions.js';
 export const getOneUser = async (req, res) => {
   const { user_email } = req.params;
   const { user } = req;
+
   const { opportunities } = await functions.getOne('opportunities', user_email);
 
   user.opportunities = opportunities;
--- controllers/users.controllers/listAllUsers.js
+++ controllers/users.controllers/listAllUsers.js
@@ -2,5 +2,6 @@ import functions from '../../database/functions.js';
 
 export const listAllUsers = async (_req, res) => {
   const users = await functions.getAll('users');
+
   return res.status(200).json(users);
 };
