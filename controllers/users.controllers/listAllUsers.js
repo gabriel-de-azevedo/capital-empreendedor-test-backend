b67d5a559031f0f243a28a049f63e9d92328244b
Gabriel de Azevedo <gmarquesazti@gmail.com>
2022-07-31 10:00:30 -0300

    fix: add simple seialization

--- controllers/users.controllers/listAllUsers.js
+++ controllers/users.controllers/listAllUsers.js
@@ -1,7 +1,9 @@
 import functions from '../../database/functions.js';
 
 export const listAllUsers = async (_req, res) => {
-  const users = await functions.getAll('users');
+  let users = await functions.getAll('users');
+
+  users = Object.values(users);
 
   return res.status(200).json({ users });
 };
