0d4b91950689150132c73f1d85367d2191c35dbd
Gabriel de Azevedo <gmarquesazti@gmail.com>
2022-08-02 10:49:49 -0300

    feat: add createUser route

---
+++ controllers/users.controllers/createUser.js
@@ -0,0 +1,10 @@
+import functions from '../../database/functions.js';
+
+export const createUser = async (req, res) => {
+  const { body } = req;
+
+  const user = await functions.set('users', body.email, body);
+  await functions.set('opportunities', body.email, { opportunities: [] });
+
+  return res.status(201).json({ user });
+};
--- routes/users.routes/usersRouter.js
+++ routes/users.routes/usersRouter.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
+import { createUser } from '../../controllers/users.controllers/createUser.js';
 import { getOneUser } from '../../controllers/users.controllers/getOneUser.js';
 import { listAllUsers } from '../../controllers/users.controllers/listAllUsers.js';
+import { validateSchema } from '../../middlewares/general.middlewares/validateSchema.js';
 import { verifyUserEmail } from '../../middlewares/users.middlewares/verifyUserEmail.js';
+import { userSchema } from '../../models/users.models/userSchema.js';
 
 /**
  * Defines the listAllUsers controller to the first route
@@ -14,5 +17,7 @@ const usersRouter = Router();
 usersRouter.get('', listAllUsers);
 
 usersRouter.get('/:user_email', verifyUserEmail, getOneUser);
+
+usersRouter.post('', validateSchema(userSchema), createUser);
 
 export default usersRouter;
