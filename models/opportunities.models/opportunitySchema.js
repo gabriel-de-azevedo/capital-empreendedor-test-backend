3d0393e64477886ce0da50a0d92022a65c2fb71a
Gabriel de Azevedo <gmarquesazti@gmail.com>
2022-08-02 10:48:57 -0300

    fix: update error messages

--- models/opportunities.models/opportunitySchema.js
+++ models/opportunities.models/opportunitySchema.js
@@ -1,9 +1,9 @@
 import * as yup from 'yup';
 
 export const opportunitySchema = yup.object().shape({
-  name: yup.string().required().typeError(),
-  limit: yup.number().required().typeError(),
-  interest: yup.number().required().typeError(),
-  term: yup.number().integer().required().typeError(),
-  isActive: yup.bool().required().typeError(),
+  name: yup.string().required().typeError('name must be a string'),
+  limit: yup.number().required().typeError('limit must be a string'),
+  interest: yup.number().required().typeError('interest must be a number'),
+  term: yup.number().integer().required().typeError('term must be a number'),
+  isActive: yup.bool().required().typeError('isActive must be a boolean'),
 });
--- models/users.models/userSchema.js
+++ models/users.models/userSchema.js
@@ -1,14 +1,17 @@
 import * as yup from 'yup';
 
 export const userSchema = yup.object().shape({
-  name: yup.string().required().typeError(),
-  email: yup.string().required().typeError().email(),
-  isActive: yup.bool().required().typeError(),
+  name: yup.string().required().typeError('name must be a string'),
+  email: yup.string().required().typeError('email must be a string').email(),
+  isActive: yup.bool().required().typeError('isActive must be a boolean'),
   phone: yup
     .string()
     .required()
-    .typeError()
-    .matches(/^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/),
-  revenue: yup.number().required().typeError(),
-  agreedTerms: yup.bool().required().typeError(),
+    .typeError('phone must be a string')
+    .matches(
+      /^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/,
+      'phone must match the following: (xx) xxxxx-xxxx'
+    ),
+  revenue: yup.number().required().typeError('revenue must be a number'),
+  agreedTerms: yup.bool().required().typeError('agreedTerms must be a boolean'),
 });
